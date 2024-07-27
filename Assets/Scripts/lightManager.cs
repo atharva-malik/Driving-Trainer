abe9a7da6e8254099ee9e746a81f2cf8f8fcdbeb
Atharva <atharva.malik@gmail.com>
2024-07-27 13:25:54 +1000

    Car game: 3

--- Assets/Scripts/lightManager.cs
+++ Assets/Scripts/lightManager.cs
@@ -7,13 +7,17 @@ public class lightManager : MonoBehaviour
 {
     public static lightManager Instance { get; set; }
     [SerializeField] private Light[] leftLights;
+    [SerializeField] private GameObject leftArrow;
     [SerializeField] private Light[] rightLights;
+    [SerializeField] private GameObject rightArrow;
     [SerializeField] private Light[] rearLights;
     [SerializeField] private Light[] frontMainLights;
 
     private int leftLightState = 0;
     private int rightLightState = 0;
     private int frontMainLightState = 0;
+
+    private float blinkerCooldown = 0f;
 
     private void Awake() {
         if (Instance != null && Instance != this)
@@ -28,14 +32,9 @@ public class lightManager : MonoBehaviour
 
     void Start()
     {
-        foreach (Light light in leftLights)
-            light.enabled = false;
-        foreach (Light light in rightLights)
-            light.enabled = false;
-        foreach (Light light in rearLights)
-            light.enabled = false;
-        foreach (Light light in frontMainLights)
-            light.enabled = false;
+        leftArrow.SetActive(false);
+        rightArrow.SetActive(false);
+        DisableAllLights();
     }
 
     void Update()
@@ -47,22 +46,29 @@ public class lightManager : MonoBehaviour
         }else if (Input.GetKeyDown(KeyCode.E)){
             EnableRightLights();
         }
-        if (leftLightState == 1){
-            if (leftLights[0].enabled){
-                leftLights[0].enabled = false;
-                leftLights[1].enabled = false;
-            }else{
-                leftLights[0].enabled = true;
-                leftLights[1].enabled = true;
+        if (blinkerCooldown >= 1){
+            if (leftLightState == 1){
+                if (leftLights[0].enabled){
+                    leftLights[0].enabled = false;
+                    leftLights[1].enabled = false;
+                    rightArrow.SetActive(false);
+                }else{
+                    leftLights[0].enabled = true;
+                    leftLights[1].enabled = true;
+                    rightArrow.SetActive(true);
+                }
+            }else if (rightLightState == 1){
+                if (rightLights[0].enabled){
+                    rightLights[0].enabled = false;
+                    rightLights[1].enabled = false;
+                }else{
+                    rightLights[0].enabled = true;
+                    rightLights[1].enabled = true;
+                }
             }
-        }else if (rightLightState == 1){
-            if (rightLights[0].enabled){
-                rightLights[0].enabled = false;
-                rightLights[1].enabled = false;
-            }else{
-                rightLights[0].enabled = true;
-                rightLights[1].enabled = true;
-            }
+            blinkerCooldown = 0f;
+        }else{
+            blinkerCooldown += Time.deltaTime;
         }
         // EnableRearLights();
         // DisableAllLights();
@@ -104,8 +110,8 @@ public class lightManager : MonoBehaviour
     {
         if (frontMainLightState == 1){
             foreach (Light light in frontMainLights)
-                // MAKE IT BRIGHTER
-            frontMainLightState = 0;
+                light.intensity = 2;
+            frontMainLightState = 2;
             return;
         }else if (frontMainLightState == 2){
             foreach (Light light in frontMainLights)
@@ -115,8 +121,23 @@ public class lightManager : MonoBehaviour
         }
         foreach (Light light in frontMainLights){
             light.enabled = true;
-            // MAKE IT DIMMER
+            light.intensity = 1;
         }
         frontMainLightState = 1;
+    }
+
+    private void DisableAllLights(){
+        foreach (Light l in leftLights){
+            l.enabled = false;
+        }
+        foreach (Light l in rightLights){
+            l.enabled = false;
+        }
+        foreach (Light l in rearLights){
+            l.enabled = false;
+        }
+        foreach (Light l in frontMainLights){
+            l.enabled = false;
+        }
     }
 }
