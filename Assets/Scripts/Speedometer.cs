d4d9b9189258f58e953cc4a7766c2b8eaab54080
Atharva <atharva.malik@gmail.com>
2024-08-04 17:31:45 +1000

    car game: 6

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -6,19 +6,35 @@ using TMPro;
 public class Speedometer : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerToKeepTrackOf;
+    [SerializeField] private bool isPhysicalSpeedometer;
 
+    [Space(10)]
+    [SerializeField] private float needleZeroRotation;
+    [SerializeField] private float needleMaxRotation;
+
+    private Player player;
     private TMP_Text text;
-    private float last_speed;
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TMP_Text>();
-        last_speed = playerToKeepTrackOf.velocity.magnitude;
+        player = playerToKeepTrackOf.gameObject.GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = Mathf.Floor(playerToKeepTrackOf.velocity.magnitude).ToString() + " mps      |       " + Mathf.Floor(playerToKeepTrackOf.velocity.magnitude * 3.6f).ToString() + " kmph";
+        if (isPhysicalSpeedometer){
+            float rot = Map(playerToKeepTrackOf.velocity.magnitude, 0, player.maxSpeed, needleZeroRotation, needleMaxRotation);
+            gameObject.transform.Find("Needle").localEulerAngles = new Vector3(0, 0, rot);
+        }
+        
+        else
+            text.text = Mathf.Floor(playerToKeepTrackOf.velocity.magnitude).ToString() + " mps      |       " + Mathf.Floor(playerToKeepTrackOf.velocity.magnitude * 3.6f).ToString() + " kmph";
+    }
+
+    public float Map (float value, float fromSource, float toSource, float fromTarget, float toTarget)
+    {
+        return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
     }
 }
